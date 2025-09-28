7782ba705e3e613a59a21a3f1c07f3b207c83d3e
Razvan Gorea <103372955+Razvan-Gorea@users.noreply.github.com>
2025-09-28 14:58:18 +0100

    Added my shell project

--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -11,19 +11,18 @@ function Projects() {
       technologies: ["Python", "JavaScript", "HTML/CSS","FastAPI", "LangGraph", "JavaScript", "React", "SQL", "Docker", "Git", "Gitlab CI"],
       github: "https://github.com/Razvan-Gorea/LangGraph-Multi-Agent-System"
     },
-    /**{
-      title: "Ireland Hiking Application", 
-      description: "A hiking web application that shows walking, jogging and cycling trails in Ireland. Each trail also displays the length along with a difficulty rating.",
-      technologies: ["Python", "JavaScript", "HTML/CSS","React", "Django", "Leaflet", "Git"],
-      github: "https://github.com/Razvan-Gorea/Trekster",
-      demo: "#"
-    },**/
     {
       title: "NASA Risk Dashboard",
       description: "An interactive dashboard leveraging NASA's Near Earth Objects API to visualize asteroid risk assessments with advanced filtering, search capabilities, and real-time data visualization.",
       technologies: ["JavaScript", "HTML", "Tailwind CSS", "React", "Node.js", "Express", "Git"],
       github: "https://github.com/Razvan-Gorea/Asteroid-Risk-Assessment-Dashboard",
       demo: "https://asteroid-risk-assessment-dashboard.onrender.com/"
+    },
+    {
+      title: "Custom Unix Shell",
+      description: "This is a custom shell implementation written in C that emulates bash functionality on Linux. The project creates an executable called myshell that provides a command-line interface similar to bash.",
+      technologies: ["C"],
+      github: "https://github.com/Razvan-Gorea/Shell-Project",
     }
   ];
 
